78c5a6f99cdccd2611b2b7e348d204eb984a53e0
gideonoludeyi <gooludeyi@gmail.com>
2023-02-23 17:20:29 -0500

    refactor `HomeScreen` widget
    extract `NoteFeedList` and `NoteFeedListItem` widgets

--- lib/main.dart
+++ lib/main.dart
@@ -55,25 +55,57 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text(widget.title),
       ),
       body: SafeArea(
-        child: ListView.builder(
-          itemCount: notes.length,
-          itemBuilder: (context, index) {
-            final note = notes[index];
-
-            return Card(
-              key: ValueKey(note.id),
-              child: ListTile(
-                title: Text(note.title),
-                subtitle: Text(note.subtitle),
-              ),
-            );
-          },
+        child: NoteFeedList(
+          notes: notes,
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Write',
         child: const Icon(Icons.edit_outlined),
+      ),
+    );
+  }
+}
+
+class NoteFeedList extends StatelessWidget {
+  const NoteFeedList({
+    super.key,
+    required this.notes,
+  });
+
+  final List<Note> notes;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: notes.length,
+      itemBuilder: (context, index) {
+        final note = notes[index];
+
+        return NoteFeedListItem(
+          key: ValueKey(note.id),
+          note: note,
+        );
+      },
+    );
+  }
+}
+
+class NoteFeedListItem extends StatelessWidget {
+  final Note note;
+
+  const NoteFeedListItem({
+    super.key,
+    required this.note,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(note.title),
+        subtitle: Text(note.subtitle),
       ),
     );
   }
